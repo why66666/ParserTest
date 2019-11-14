914b04ee2ea57cab040489840a061014ad244fc9
why66666 <why_519@163.com>
2019-11-15 02:18:07 +0800

    Stack + Tree 工具

---
+++ src/Utils.java
@@ -0,0 +1,22 @@
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.tree.JCTree;
+
+import java.util.Stack;
+
+public class Utils {
+    public Utils() {}
+
+    public static boolean isSameTree(Tree tree1, Tree tree2){
+        return ((JCTree) tree1).pos == ((JCTree) tree2).pos;
+    }
+
+    public static boolean hasTree(Stack<Tree> stack,Tree tree){
+        while (!stack.empty()){
+            if(isSameTree(stack.pop(),tree)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
